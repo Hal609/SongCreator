112230ee8c7949c940e876d6f87468698a3d8635
Hal609 <hal@kolb.co.uk>
2023-04-01 20:02:55 +0100

    Add variously pitched notes to be added on top of generated track

--- songCreator.py
+++ songCreator.py
@@ -1,5 +1,34 @@
 from pydub import AudioSegment
 import numpy as np
+import librosa
+
+def pitch_shift(sound, n_half_steps):
+    octaves = n_half_steps / 12
+
+    new_sample_rate = int(sound.frame_rate * (2.0 ** octaves))
+
+    lowpitch_sound = sound._spawn(sound.raw_data, overrides={'frame_rate': new_sample_rate})
+
+    # y = np.frombuffer(sound._data, dtype=np.int16).astype(np.float32)/2**15
+    # y = librosa.effects.pitch_shift(y, sound.frame_rate, n_steps=n_steps)
+    # a  = AudioSegment(np.array(y * (1<<15), dtype=np.int16).tobytes(), frame_rate = sound.frame_rate, sample_width=2, channels = 1)
+    return lowpitch_sound
+
+def addMelody(note_list):
+    # Create an empty AudioSegment to hold the beat
+    audio = AudioSegment.empty()
+
+    # Create the beat based on the beat list
+    for i in range(len(note_list)):
+        # beat_type = beat_list[i]
+        if i < 4:
+            hit = lead
+        else:
+            hit = pitch_shift(lead, 4)
+        audio += silence
+        audio = audio.overlay(hit, position=len(audio)-len(silence))
+
+    return audio
 
 def addTrack(beat_list):
     # Define a dictionary mapping each beat type to its corresponding sample
@@ -30,15 +59,23 @@ kick = AudioSegment.from_file(samplePath + "Kick_1_808Flex.aif")
 snare = AudioSegment.from_file(samplePath + "Snare_1_808Flex.aif")
 hihat = AudioSegment.from_file(samplePath + "Hi-Hat_808Flex.aif")
 ride = AudioSegment.from_file(samplePath + "Ride_808Flex.aif")
+leadPath =  "/Library/Application Support/Logic/Alchemy Samples/Keys/Electric Pianos/EPiano FM Classic/"
+lead = AudioSegment.from_file(leadPath + "EPiano FM Classic C3.wav")
 silence = AudioSegment.silent(duration=len(silence))
 
 
 tracks = [
     [1, 0, 2, 2, 0, 0, 2, 0, 1, 0, 2, 2, 0, 0, 2, 0],
-    [3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3]
+    [3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3],
 ]
 
+
 fullBeat = addTrack(list(np.zeros(len(tracks[0]))))
+
+noteTrack = list(np.zeros(len(tracks[0])))
+for i in range(len(noteTrack)):
+    noteTrack[i] = "A4"
+fullBeat = fullBeat.overlay(addMelody(noteTrack))
 
 for beat in tracks:
     fullBeat = fullBeat.overlay(addTrack(beat))
