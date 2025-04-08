27d7c55ab70f7f2ae9a99e144360b48d18809ceb
Hal Kolb <hal@kolb.co.uk>
2025-04-08 10:35:02 +0100

    Rename main to generateData

---
+++ generateData.py
@@ -0,0 +1,9 @@
+import src
+
+choice = input("What type of data do you want to create ?\n    1. NMultiInstSpectrograms\n      2. SplitTrackSpectrograms")
+if choice == 1:
+   src.createNMultiInstSpectrograms(5000, keepWAVs=False)
+elif choice == 2:
+   src.createSplitTrackSpectrograms(5000, keepWAVs=False)
+else:
+   print("Selection not recognised.")
--- main.py
+++
@@ -1,4 +0,0 @@
-import src
-
-# src.createNMultiInstSpectrograms(5000, keepWAVs=False)
-src.createSplitTrackSpectrograms(5000, keepWAVs=False)
