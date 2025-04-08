278d634a2b2381873ebf3c1fbc65071957e03e51
Hal Kolb <hal@kolb.co.uk>
2025-04-08 13:17:05 +0100

    Restore multi-instrument spectrogram creation code

--- src/generateMultiInstSpectrograms.py
+++ src/generateMultiInstSpectrograms.py
@@ -4,7 +4,6 @@ import random as rand
 from os import path
 from pydub import AudioSegment
 import pathlib
-from pretty_progress import progress_bar
 
 from .noteClass import Note
 from .globals import *
@@ -86,26 +85,15 @@ def addTrack(beat_list, silence):
     return beat
 
 def countCurrentSpectrograms():
-    counts = {
-        "piano": 0, 
-        "bass": 0, 
-        "drums": 0, 
-        "guitar": 0, 
-        "synth": 0
-    }
-    for inst in counts.keys():
-        counts[inst] = len([name for name in os.listdir(f'./dataset_root/{inst}')])
-        print(f"{inst}:", counts[inst])
-        
-    return counts
+    return len([name for name in os.listdir('data/spectrograms')])
 
 def createNMultiInstSpectrograms(n, keepWAVs = False):
     global kick, snare, hihat, ride
 
-    counts = countCurrentSpectrograms() 
+    count = countCurrentSpectrograms()
 
     for i in range(n):
-        print(i)
+        print(count)
         
         bpm = int(np.random.normal(108, 20, 1)[0])     # 1 point with mean 108 and sd 20
 
@@ -113,32 +101,6 @@ def createNMultiInstSpectrograms(n, keepWAVs = False):
         interval = (1/bpm * 60 * 1000) / 2
 
         silence = AudioSegment.silent(duration=interval)
-
-        # Step 1: Pick an instrument to create a spectrogram for
-        inst = rand.choice(instruments)
-        counts[inst] += 1
-
-        # Step 2: Pick a voice for the chosen instrument
-        voice = rand.choice(voices[inst])
-
-        # Step 3: Save the path to the chosen instrument samples
-        instSamplePath = f"samples/{inst}/{voice}/"
-
-        if inst == "drums":
-            kick = AudioSegment.from_file(instSamplePath + "Kick.wav")
-            snare = AudioSegment.from_file(instSamplePath + "Snare.wav")
-            hihat = AudioSegment.from_file(instSamplePath + "Hi-Hat.wav")
-            ride = AudioSegment.from_file(instSamplePath + "Ride.wav")
-
-        # Step 4: Select what sort of playing to create
-        num_items = rand.randint(1, len(parts_can_add[inst]))
-        if inst in ["guitar", "bass"]: num_items = 1
-        selected_items = rand.sample(parts_can_add[inst], num_items)
-        if inst in ["synth", "piano"] and "melody" in selected_items and "chords" in selected_items:
-            if rand.random() < 0.5:
-                selected_items.remove("chords")
-            else:
-                selected_items.remove("melody")
 
         totalLength = 12*8
 
@@ -165,22 +127,69 @@ def createNMultiInstSpectrograms(n, keepWAVs = False):
             nextNote = melodyNotes[j % len(melodyNotes)]
             fullMelodyNotes.append(nextNote)
             fullBassNotes.append(bassNotes[j % len(bassNotes)])
+        
+        count += 1
+        
+        num_instruments = rand.randint(2, 5)
+        instrumentTracks = {}
+        trackPaths = {}
+        unused_insts = ["piano", "bass", "drums", "guitar", "synth"]
+        used_insts = []
+        for k in range(num_instruments):
+            # Step 1: Pick an instrument to create a spectrogram for
+            inst = rand.choice(unused_insts)
+            unused_insts.remove(inst)
+            used_insts.append(inst)
 
-        if "melody" in selected_items:
-            fullBeat = fullBeat.overlay(addMelody(fullMelodyNotes, instSamplePath, silence))
-        if "chords" in selected_items:
-            fullBeat = fullBeat.overlay(addChords(key, chordProgression, instSamplePath, silence))
-        if "bass" in selected_items:
-            fullBeat = fullBeat.overlay(addMelody(fullBassNotes, instSamplePath, silence))
-        if "drums" in selected_items:
+            # Step 2: Pick a voice for the chosen instrument
+            voice = rand.choice(voices[inst])
+
+            # Step 3: Save the path to the chosen instrument samples
+            instSamplePath = f"samples/{inst}/{voice}/"
+
+            if inst == "drums":
+                kick = AudioSegment.from_file(instSamplePath + "Kick.wav")
+                snare = AudioSegment.from_file(instSamplePath + "Snare.wav")
+                hihat = AudioSegment.from_file(instSamplePath + "Hi-Hat.wav")
+                ride = AudioSegment.from_file(instSamplePath + "Ride.wav")
+
+            # Step 4: Select what sort of playing to create
+            num_items = rand.randint(1, len(parts_can_add[inst]))
+            if inst in ["guitar", "bass"]: num_items = 1
+            selected_items = rand.sample(parts_can_add[inst], num_items)
+            if inst in ["synth", "piano"] and "melody" in selected_items and "chords" in selected_items:
+                if rand.random() < 0.5:
+                    selected_items.remove("chords")
+                else:
+                    selected_items.remove("melody")
+
+            if "melody" in selected_items:
+                fullBeat = fullBeat.overlay(addMelody(fullMelodyNotes, instSamplePath, silence))
+            if "chords" in selected_items:
+                fullBeat = fullBeat.overlay(addChords(key, chordProgression, instSamplePath, silence))
+            if "bass" in selected_items:
+                fullBeat = fullBeat.overlay(addMelody(fullBassNotes, instSamplePath, silence))
+            if "drums" in selected_items:
                 for beat in tracks:
                     fullBeat = fullBeat.overlay(addTrack(beat, silence))
 
-        filepath = path.dirname(__file__) + f"/../temp/{inst}_{voice}_{counts[inst]:04}.wav"
-        # print(f"Exporting wav to {filepath}.")
+        filepath = path.dirname(__file__) + f"/../temp/track{count:04}_full.wav"
         fullBeat.export(filepath)
-        # print("Export finished")
-
+        
         gloPath = pathlib.Path(__file__).parent.resolve()
-        wavToSpect(filepath, f"{gloPath}/../dataset_root/{inst}/{inst}_{counts[inst]:04}.bmp")
+        newPath = f"{gloPath}/../data/spectrograms/track{count:04}.bmp"
+        wavToSpect(filepath, newPath)
         if not keepWAVs: os.remove(filepath)
+
+        # filename,piano,bass,drums,guitar,synth
+        res = ""
+        for inst in ["piano", "bass", "drums", "guitar", "synth"]:
+            if inst in used_insts:
+                res += ",0"
+            else:
+                res += ",1"
+
+        text = f"\ntrack{count:04}.bmp" + res
+        with open("data/labels.csv", "a") as csv:
+            csv.write(text)
+
